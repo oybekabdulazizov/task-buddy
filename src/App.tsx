cde2225d3bcc0bd1022909f9058090626d6e3ce1
Oybek Abdulazizov <oybek.tulqinovich@gmail.com>
2023-10-25 15:18:49 +0200

    fix the height and width of the app

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import TodosList from './components/TodosList';
 
 export default function App() {
   return (
-    <div className='w-full h-full py-10 bg-sky-950'>
+    <div className='min-h-full py-10 px-4 bg-sky-950'>
       <div className='mx-auto max-w-[500px] py-4 px-6 bg-stone-300 rounded-lg'>
         <h1 className='text-4xl font-light my-4'>TaskBuddy</h1>
         <TodoForm />
