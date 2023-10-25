1fb9f62168ed72bfb67935399765fe1bc00d3288
Oybek Abdulazizov <oybek.tulqinovich@gmail.com>
2023-10-25 18:56:16 +0200

    slightly expand the app width

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import TodosList from './components/TodosList';
 export default function App() {
   return (
     <div className='min-h-full py-10 px-4 bg-sky-950'>
-      <div className='mx-auto max-w-[500px] py-4 px-6 bg-stone-300 rounded-lg'>
+      <div className='mx-auto max-w-[600px] py-4 px-6 bg-stone-300 rounded-lg'>
         <h1 className='text-4xl font-light my-4'>TaskBuddy</h1>
         <CreateTodo />
         <TodosList />
