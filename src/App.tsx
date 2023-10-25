6bbbdb5371d7eae57791d092330db86787d0d618
Oybek Abdulazizov <oybek.tulqinovich@gmail.com>
2023-10-25 14:19:44 +0200

    remove App.css, and improve the look of App

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
-import './App.css';
 import TodosList from './components/TodosList';
 
 export default function App() {
   return (
-    <div className='bg-red-300 mx-auto w-[900px] h-full pt-6 py-10'>
-      <h1 className='text-4xl font-light my-6'>TaskBuddy</h1>
-      <TodosList />
+    <div className='w-full h-full py-10 bg-sky-950'>
+      <div className='mx-auto max-w-[500px] py-4 px-6 bg-stone-300 rounded-lg'>
+        <h1 className='text-4xl font-light my-4'>TaskBuddy</h1>
+        <TodosList />
+      </div>
     </div>
   );
 }
--- src/components/TodosList.tsx
+++ src/components/TodosList.tsx
@@ -6,7 +6,7 @@ export default function TodosList() {
   const todos = useSelector(getTodos);
 
   return (
-    <div>
+    <div className='my-2'>
       {todos.map((todo: todo_type) => (
         <div key={todo.id}>{todo.task}</div>
       ))}
