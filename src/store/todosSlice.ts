67c3aa8a771cffad8c1c908ec5d6176f5c594a09
Oybek Abdulazizov <oybek.tulqinovich@gmail.com>
2023-10-25 14:35:25 +0200

    add createdAt and completed fields

--- src/store/todosSlice.ts
+++ src/store/todosSlice.ts
@@ -3,11 +3,23 @@ import { createSlice, nanoid } from '@reduxjs/toolkit';
 export type todo_type = {
   id: string;
   task: string;
+  createdAt: number;
+  completed: boolean;
 };
 
 const initialState: Array<todo_type> = [
-  { id: '11ww22', task: 'Mow the lawn' },
-  { id: '33ff44', task: 'Feed the kitty' },
+  {
+    id: '11ww22',
+    task: 'Mow the lawn',
+    createdAt: 1697967657095,
+    completed: false,
+  },
+  {
+    id: '33ff44',
+    task: 'Feed the kitty',
+    createdAt: 1697967210000,
+    completed: true,
+  },
 ];
 
 const todosSlice = createSlice({
@@ -23,6 +35,8 @@ const todosSlice = createSlice({
           payload: {
             id: nanoid(),
             task,
+            createdAt: Date.now(),
+            completed: false,
           },
           meta: null,
           error: null,
