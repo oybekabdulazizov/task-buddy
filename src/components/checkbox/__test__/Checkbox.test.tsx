d1ac87af01694b9e73358b7d6d1f70eea3e7e8f4
Oybek Abdulazizov <oybek.tulqinovich@gmail.com>
2023-11-02 17:14:42 +0100

    final touches

--- src/components/checkbox/__test__/Checkbox.test.tsx
+++ src/components/checkbox/__test__/Checkbox.test.tsx
@@ -1,5 +1,6 @@
 import { fireEvent, render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
+
 import Checkbox from '../Checkbox';
 
 describe('reusable checkbox test', () => {
